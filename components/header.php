91d6c09a799d41edaf472484e2dc18ab5a78d451
nikita781 <78200828+nikita781@users.noreply.github.com>
2024-04-16 18:53:53 +0400

    настройка проект и шапка

---
+++ components/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/index.css">
+    <title>Салон красоты</title>
+</head>
+<body>
+    <div class="header" id="header">
+        <div class="header__head">
+            <a href="tel:+71234567890" class="header__phone-container">
+                <img src="/assets/img/call.svg" alt="call">
+                <p class="header__phone">+7 (123) 456 78 90</p>
+            </a>
+            <div class="header__contact">
+                <div class="header__contact-callback">
+                    <img src="/assets/img/callback.svg" alt="call">
+                    <p>Обратный звонок</p>
+                </div>
+                <div class="header__contact-callback">
+                    <img src="/assets/img/mail.svg" alt="callback">
+                    <p>Напишите нам</p>
+                </div>
+            </div>
+        </div>
+        <div class="header__main">
+            <div class="header__logo">
+                <img src="/assets/img/logo.svg" alt="logo">
+                <p class="header__logo-name">Название</p>
+            </div>
+            <div class="header__menu">
+                <ul>
+                    <li>Страница</li>
+                    <li>Страница</li>
+                    <li>Страница</li>
+                    <li>Страница</li>
+                    <li>Страница</li>
+                </ul>
+            </div>
+            <div id="burger_button" class="header__burger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+        <div id="menu_container" class="header__menu-phone">
+            <div class="header__small">
+                <a href="tel:+71234567890" class="header__phone-container">
+                    <img src="/assets/img/call.svg" alt="call">
+                    <p class="header__phone">+7 (123) 456 78 90</p>
+                </a>
+                <div class="header__contact-callback">
+                    <img src="/assets/img/callback.svg" alt="call">
+                    <p>Обратный звонок</p>
+                </div>
+                <div class="header__contact-callback">
+                    <img src="/assets/img/mail.svg" alt="callback">
+                    <p>Напишите нам</p>
+                </div>
+                <div class="header__small-menu">
+                    <ul>
+                        <li>Страница</li>
+                        <li>Страница</li>
+                        <li>Страница</li>
+                        <li>Страница</li>
+                        <li>Страница</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ js/header.js
@@ -0,0 +1,25 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  if (window.innerWidth > 768) {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.querySelector(".header").style.top = "0";
+    } else {
+      document.querySelector(".header").style.top = "-123px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
+}
+
+const burger_button = document.querySelector('#burger_button');
+const menu_container = document.querySelector('#menu_container');
+burger_button.addEventListener('click', () => {
+    burger_button.classList.toggle('burger_active');
+    menu_container.classList.toggle('menu_active');
+    document.body.classList.toggle('scroll-lock');
+    if (menu_container.classList.contains('menu_active')) {
+        disableScroll = true;
+    } else {
+        disableScroll = false;
+    }
+}) 
