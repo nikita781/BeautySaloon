8d7ca327c6ae0c529dbc76f9ddfc6f15c174c115
20vlad052 <mr.ralenskiy@mail.ru>
2024-04-22 22:52:19 +0300

    все заебись

--- components/miniblog.php
+++ components/miniblog.php
@@ -6,7 +6,7 @@
                 <img src="/assets/img/slide-test.png" alt="">
                 <div class="miniblog_container-info">
                     <h3>ТЕСТОВАЯ ЗАПИСЬ | #1</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus laudantium, nam magni esse amet provident dolor cumque consectetur id quo. Quam vel nam architecto laudantium, voluptatibus et ducimus illo porro.</p>
+                    <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus laudantium, nam magni esse amet provident dolor cumque consectetur id quo. Quam vel nam architecto laudantium, voluptatibus et ducimus illo porro.</p>
                     <p>3 августа, 2022</p>
                 </div>
             </a>
